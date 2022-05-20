dad33dbef968ae60204dfc01d7f57b87c427b888
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-05-20 17:52:51 +0200

    videochat is done

--- public/script.js
+++ public/script.js
@@ -3,6 +3,13 @@ const videoGrid = document.querySelector('#video-grid')
 const myVideo = document.createElement('video')
 const showChat = document.querySelector('#showChat')
 const backBtn = document.querySelector('.header__back')
+const inviteButton = document.querySelector('#inviteButton')
+const stopVideo = document.querySelector('#stopVideo')
+const muteButton = document.querySelector('#muteButton')
+const messageForm = document.querySelector('form')
+const message = document.querySelector('#chat_message')
+const sendMessage = document.querySelector('#send')
+const messages = document.querySelector('.messages')
 myVideo.muted = true
 
 const user = prompt('Enter your name')
@@ -36,6 +43,74 @@ navigator.mediaDevices
     });
   });
 
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id, user);
+});
+
+muteButton.addEventListener('click', () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled
+
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false
+        html = `<i class="fas fa-microphone-slash"></i>`
+        muteButton.classList.toggle('background__red')
+        muteButton.innerHTML = html
+    } else {
+        myVideoStream.getAudioTracks()[0].enabled = true
+        html = `<i class="fas fa-microphone"></i>`
+        muteButton.classList.toggle('background__red')
+        muteButton.innerHTML = html
+    }
+})
+
+stopVideo.addEventListener('click', () => {
+    const enabled = myVideoStream.getVideoTracks()[0].enabled
+
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false
+        html = `<i class="fas fa-video-slash"></i>`
+        stopVideo.classList.toggle('background__red')
+        stopVideo.innerHTML = html
+    } else {
+        myVideoStream.getVideoTracks()[0].enabled = true
+        html = `<i class="fas fa-video"></i>`
+        stopVideo.classList.toggle('background__red')
+        stopVideo.innerHTML = html
+    }
+})
+
+inviteButton.addEventListener('click', e => {
+    prompt(
+        'Copy this link and send it to people you want to meet with',
+        window.location.href
+    )
+})
+
+sendMessage.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    if (message.value.length !== 0) {
+        socket.emit('message', message.value)
+        message.value = ''
+    } 
+})
+
+socket.on('createMessage', (message, userName) => {
+    const item = document.createElement('li')
+    item.classList.add('message')
+
+    console.log('message send')
+
+    item.innerHTML = `
+    <b><i class="far fa-user-circle"></i> <span> ${
+        userName === user ? "me" : userName
+      }</span> </b>
+      <span>${message}</span>
+    `
+
+    messages.appendChild(item)
+})
+
 const connectToNewUser = (userId, stream) => {
   const call = peer.call(userId, stream);
   const video = document.createElement("video");
@@ -43,10 +118,6 @@ const connectToNewUser = (userId, stream) => {
     addVideoStream(video, userVideoStream);
   });
 };
-
-peer.on("open", (id) => {
-  socket.emit("join-room", ROOM_ID, id, user);
-});
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
